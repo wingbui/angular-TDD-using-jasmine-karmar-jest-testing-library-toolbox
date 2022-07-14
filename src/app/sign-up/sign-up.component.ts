7959c9d3f0ba878bd7713e78f4e710806e8f5dd2
Wing Bui <38932460+wingbui@users.noreply.github.com>
2022-07-14 08:08:46 -0700

    build: implement fetch request to sign up

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up.component.css'],
 })
 export class SignUpComponent implements OnInit {
+  username = '';
+  email = '';
   password: string = '';
   passwordRepeat: string = '';
 
@@ -13,12 +15,33 @@ export class SignUpComponent implements OnInit {
 
   ngOnInit(): void {}
 
+  onChangeUsername(event: Event) {
+    this.username = (event.target as HTMLInputElement)?.value;
+  }
+
+  onChangeEmail(event: Event) {
+    this.email = (event.target as HTMLInputElement)?.value;
+  }
+
   onChangePassword(event: Event) {
     this.password = (event.target as HTMLInputElement)?.value;
   }
 
   onChangePasswordRepeat(event: Event) {
     this.passwordRepeat = (event.target as HTMLInputElement)?.value;
+  }
+
+  onClickSignUp() {
+    console.log('click');
+    fetch('/api/v1/signUp', {
+      method: 'POST',
+      body: JSON.stringify({
+        username: this.username,
+        email: this.email,
+        password: this.password,
+      }),
+      headers: { 'Content-type': 'application/json' },
+    });
   }
 
   isDisabled(): boolean {
