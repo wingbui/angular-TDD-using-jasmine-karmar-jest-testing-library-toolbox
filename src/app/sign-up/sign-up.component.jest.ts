7fe944317aef02b7a1390d712d9a6cd441f1d98d
Wing Bui <38932460+wingbui@users.noreply.github.com>
2022-07-15 07:45:16 -0700

    build: mock server

--- src/app/sign-up/sign-up.component.jest.ts
+++ src/app/sign-up/sign-up.component.jest.ts
@@ -1,57 +1,77 @@
-import { screen, render, getByRole } from '@testing-library/angular';
+import { screen, render } from '@testing-library/angular';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import userEvent from '@testing-library/user-event';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+
 import 'whatwg-fetch';
 
 import { SignUpComponent } from './sign-up.component';
+import { TestBed } from '@angular/core/testing';
+
+const server = setupServer(
+  rest.post('/api/v1/users', (req, res, ctx) => {
+    return res(ctx.status(201), ctx.json({}));
+  })
+);
+
+const setup = async () => {
+  await render(SignUpComponent, {
+    imports: [HttpClientTestingModule],
+  });
+};
 
 describe('Layout', () => {
   it('should have a header h1', async () => {
-    await render(SignUpComponent);
-    const h1 = screen.getByRole('heading', { name: 'Sign Up' });
-    expect(h1).toBeInTheDocument();
+    await setup();
+    expect(
+      screen.getByRole('heading', { name: 'Sign Up' })
+    ).toBeInTheDocument();
   });
 
   it('should have a Username input', async () => {
-    await render(SignUpComponent);
+    await setup();
     expect(screen.getByLabelText('Username')).toBeInTheDocument();
   });
 
   it('should have a Email input', async () => {
-    await render(SignUpComponent);
+    await setup();
     expect(screen.getByLabelText('Email')).toBeInTheDocument();
   });
 
   it('should have a Password input', async () => {
-    await render(SignUpComponent);
+    await setup();
     expect(screen.getByLabelText('Password')).toBeInTheDocument();
   });
 
   it('should have a password type for Password input', async () => {
-    await render(SignUpComponent);
-    const input = screen.getByLabelText('Password');
-    expect(input).toHaveAttribute('type', 'password');
+    await setup();
+    expect(screen.getByLabelText('Password')).toHaveAttribute(
+      'type',
+      'password'
+    );
   });
 
   it('should have Sign Up button', async () => {
-    await render(SignUpComponent);
-    const button = screen.getByRole('button', { name: 'Sign Up' });
-    expect(button).toBeInTheDocument();
+    await setup();
+    expect(screen.getByRole('button', { name: 'Sign Up' })).toBeInTheDocument();
   });
 
   it('should disable the button initially', async () => {
-    await render(SignUpComponent);
-    const button = screen.getByRole('button', { name: 'Sign Up' });
-
-    expect(button).toBeDisabled();
+    await setup();
+    expect(screen.getByRole('button', { name: 'Sign Up' })).toBeDisabled();
   });
 });
 
 describe('Interaction', () => {
   it('it should enable the button when Password and Repeat Password match', async () => {
-    await render(SignUpComponent);
+    await setup();
     const inputPassword = screen.getByLabelText('Password');
     const inputPasswordRepeat = screen.getByLabelText('Password Repeat');
-    const user = userEvent.setup();
+    const user = userEvent;
     await user.type(inputPassword, 'p@ssword');
     await user.type(inputPasswordRepeat, 'p@ssword');
 
@@ -61,15 +81,15 @@ describe('Interaction', () => {
   });
 
   it('should send username, email and password to backend after clicking Sign Up button', async () => {
-    const spy = jest.spyOn(window, 'fetch');
+    await setup();
 
-    await render(SignUpComponent);
+    const httpTestingController = TestBed.inject(HttpTestingController);
     const usernameInput = screen.getByLabelText('Username');
     const emailInput = screen.getByLabelText('Email');
     const passwordInput = screen.getByLabelText('Password');
     const passwordRepeatInput = screen.getByLabelText('Password Repeat');
 
-    const user = userEvent.setup();
+    const user = userEvent;
     await user.type(usernameInput, 'username');
     await user.type(emailInput, 'email');
     await user.type(passwordInput, 'password');
@@ -78,15 +98,13 @@ describe('Interaction', () => {
     const button = screen.getByRole('button', { name: 'Sign Up' });
     await userEvent.click(button);
 
-    const args = spy.mock.calls[0];
-    const secondParam = args[1];
+    const req = httpTestingController.expectOne('/api/v1/users');
+    const reqBody = req.request.body;
 
-    expect(secondParam?.body).toEqual(
-      JSON.stringify({
-        username: 'username',
-        email: 'email',
-        password: 'password',
-      })
-    );
+    expect(reqBody).toEqual({
+      username: 'username',
+      email: 'email',
+      password: 'password',
+    });
   });
 });
