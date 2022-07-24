0b2085343b9cf6f1911cfcefd010ccc7d307e296
Wing Bui <38932460+wingbui@users.noreply.github.com>
2022-07-24 07:14:40 -0700

    test: jasmine - routing to / showing home page

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,25 +1,42 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { routes } from './router/app-router/app-router.module';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let router: Router;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
+      imports: [RouterTestingModule.withRoutes(routes)],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    router = TestBed.inject(Router);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
-  it(`should have as title 'Angular TDD'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('Angular TDD');
+  it('should have as title "Angular TDD"', () => {
+    expect(component.title).toEqual('Angular TDD');
   });
 
+  describe('Routing', () => {
+    it('should navigate to home page at /', async () => {
+      await router.navigate(['/']);
+      fixture.detectChanges();
+
+      const home = fixture.nativeElement as HTMLElement;
+      expect(home.querySelector('[data-testid="home-page"]')).toBeTruthy();
+    });
+  });
 });
