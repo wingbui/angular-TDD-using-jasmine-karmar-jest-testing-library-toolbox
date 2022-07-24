5997f3aa32470a4fdc27f730727bf4433d80a8b3
Wing Bui <38932460+wingbui@users.noreply.github.com>
2022-07-24 08:19:14 -0700

    test: jest - routing to / and /sign-up

---
+++ src/app/app.component.jest.ts
@@ -0,0 +1,30 @@
+import { AppComponent } from './app.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { render, screen } from '@testing-library/angular';
+
+import { HomeComponent } from './home/home.component';
+import { routes } from './router/app-router/app-router.module';
+import { SignUpComponent } from './sign-up/sign-up.component';
+
+describe('Routing', () => {
+  it('should navigate to home page at /', async () => {
+    const { navigate } = await render(AppComponent, {
+      routes: routes,
+    });
+
+    await navigate('/');
+    expect(screen.getByTestId('home-page')).toBeInTheDocument();
+  });
+
+  it('should navigate to sign-up page at /sign-up', async () => {
+    const { navigate } = await render(AppComponent, {
+      declarations: [HomeComponent, SignUpComponent],
+      imports: [HttpClientTestingModule, ReactiveFormsModule],
+      routes: routes,
+    });
+
+    await navigate('/sign-up');
+    expect(screen.getByTestId('sign-up-page')).toBeInTheDocument();
+  });
+});
