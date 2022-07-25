2bf139fda837f769eb32573f1254c9079f02b4be
Wing Bui <38932460+wingbui@users.noreply.github.com>
2022-07-25 10:37:04 -0700

    build: login and user pages

--- src/app/router/app-router/app-router.module.ts
+++ src/app/router/app-router/app-router.module.ts
@@ -3,10 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from '../../home/home.component';
 import { SignUpComponent } from '../../sign-up/sign-up.component';
+import { LoginComponent } from '../../login/login.component';
+import { UserComponent } from '../../user/user.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'sign-up', component: SignUpComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'user', component: UserComponent },
 ];
 
 @NgModule({
