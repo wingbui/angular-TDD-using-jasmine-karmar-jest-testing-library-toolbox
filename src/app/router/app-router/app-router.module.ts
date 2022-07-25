0b8240c694de6955a40d81fec4e9486523c387e1
Wing Bui <38932460+wingbui@users.noreply.github.com>
2022-07-25 10:46:55 -0700

    fix: router with dynamic user id

--- src/app/router/app-router/app-router.module.ts
+++ src/app/router/app-router/app-router.module.ts
@@ -10,7 +10,7 @@ export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'sign-up', component: SignUpComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'user', component: UserComponent },
+  { path: 'user/:id', component: UserComponent },
 ];
 
 @NgModule({
