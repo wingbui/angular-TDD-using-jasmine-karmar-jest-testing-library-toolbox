9a107c9b97ec37032f7248645c944a455ce2dfbc
Wing Bui <38932460+wingbui@users.noreply.github.com>
2022-07-25 10:36:55 -0700

    test: jest - use 'it.each' to add test for login and user pages

--- src/app/app.component.jest.ts
+++ src/app/app.component.jest.ts
@@ -4,15 +4,32 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { render, screen } from '@testing-library/angular';
 
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 import { routes } from './router/app-router/app-router.module';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { UserComponent } from './user/user.component';
 
 describe('Routing', () => {
-
   it.each`
-   
+    path        | pageId
+    ${'/login'} | ${'login-page'}
+    ${'/user'}  | ${'user-page'}
+  `(`should navigate to the $pageId at $path`, async ({ path, pageId }) => {
+    const { navigate } = await render(AppComponent, {
+      declarations: [
+        HomeComponent,
+        LoginComponent,
+        UserComponent,
+        SignUpComponent,
+      ],
+      imports: [HttpClientTestingModule, ReactiveFormsModule],
+      routes: routes,
+    });
 
-  `
+    await navigate(path);
+    expect(screen.getByTestId(pageId)).toBeInTheDocument();
+  });
+
   it('should navigate to home page at /', async () => {
     const { navigate } = await render(AppComponent, {
       routes: routes,
